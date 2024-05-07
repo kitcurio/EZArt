0313104cff2380788e3a781fa882e30d3c1083f9
kitcurio <kasiarivers@gmail.com>
2024-05-07 11:39:18 -0400

    Initial Commit

---
+++ EZArt/EZArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZArtApp.swift
+//  EZArt
+//
+//  Created by Kasia Rivers on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct EZArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
